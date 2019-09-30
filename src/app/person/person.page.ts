dcb72cc0abd7ca6aeb00e737198633c4514f8ee7
公来 周 <zhougonglai@dingtalk.com>
2019-09-30 17:41:19 +0800

    type: update

--- src/app/person/person.page.ts
+++ src/app/person/person.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-person',
-  templateUrl: './person.page.html',
-  styleUrls: ['./person.page.scss'],
+	selector: 'app-person',
+	templateUrl: './person.page.html',
+	styleUrls: ['./person.page.scss'],
 })
 export class PersonPage implements OnInit {
+	user = {
+		avatar: 'https://api.adorable.io/avatars/400/zhougonglai@dingtalk.com',
+		nickname: 'John Zhou',
+		email: 'zhougonglai@dingtalk.com',
+	};
 
-  constructor() { }
+	wallet = {
+		balance: 52.12,
+	};
 
-  ngOnInit() {
-  }
+	constructor() {}
 
+	ngOnInit() {}
 }
